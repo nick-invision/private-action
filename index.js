1c956ed0bfd3e3be625d98acbb229bf916645389
Nick Fields <nickfields@invisionapp.com>
2019-11-20 00:19:48 -0500

    .

--- index.js
+++ index.js
@@ -1,15 +1,65 @@
+const exec = require('@actions/exec');
 const core = require('@actions/core');
-const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+const GITHUB_TOKEN = core.getInput('github-token');
+const GITHUB_REPO = core.getInput('github-repo');
+
+async function run() {
+
+  core.debug('docker version');
+  await exec.exec('docker', [
+    'version',
+  ]);
+
+  core.debug('docker status');
+  await exec.exec('sudo', [
+    'systemctl',
+    'status',
+    'docker',
+  ]);
+
+  // // buildx
+  // await exec.exec('docker', [
+  //   'buildx',
+  //   'version',
+  // ]).then(async () => {
+  //   if (DOCKER_BUILDX !== 'true') {
+  //     core.debug('buildx disabled');
+  //     return;
+  //   }
+
+  //   // install buildx
+  //   await exec.exec('docker', [
+  //     'run',
+  //     '--rm',
+  //     '--privileged',
+  //     'docker/binfmt:820fdd95a9972a5308930a2bdfb8573dd4447ad3',
+  //   ]);
+
+  //   await exec.exec('cat', [
+  //     '/proc/sys/fs/binfmt_misc/qemu-aarch64',
+  //   ]);
+
+  //   await exec.exec('docker', [
+  //     'buildx',
+  //     'create',
+  //     '--use',
+  //     '--name',
+  //     'mybuilder',
+  //   ]);
+
+  //   await exec.exec('docker', [
+  //     'buildx',
+  //     'inspect',
+  //     '--bootstrap'
+  //   ]);
+  // }, () => {
+  //   core.debug('NOT Support Buildx');
+  // });
 }
+
+run().then(() => {
+  console.log('Run success');
+}).catch((e) => {
+  core.setFailed(e.toString());
+});
