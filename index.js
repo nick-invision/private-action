10a7cb94101e4763142b472fabf9a6d3e614dbbc
Nick Fields <nickfields@invisionapp.com>
2019-11-20 09:15:59 -0500

    .

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const exec = require('@actions/exec');
 const core = require('@actions/core');
+const git = require('simple-git');
 
-const GITHUB_TOKEN = core.getInput('github-token');
-const GITHUB_REPO = core.getInput('github-repo');
+const GITHUB_TOKEN = core.getInput('github-token', {required: true});
+const GITHUB_REPO = core.getInput('github-repo', {required: true});
 
 async function run() {
 
   const envs = Object.keys(process.env).filter(e=>e.startsWith('INPUT_'));
 
-  console.log(envs)
+  core.debug(envs)
+
+  core.debug('workspace')
+  await exec.exec('ls -a')
+  
+  core.debug('PWD from action')
+  await exec.exec('echo $PWD')
 
   core.debug('docker version');
   await exec.exec('docker', [
@@ -21,45 +28,6 @@ async function run() {
     'status',
     'docker',
   ]);
-
-  // // buildx
-  // await exec.exec('docker', [
-  //   'buildx',
-  //   'version',
-  // ]).then(async () => {
-  //   if (DOCKER_BUILDX !== 'true') {
-  //     core.debug('buildx disabled');
-  //     return;
-  //   }
-
-  //   // install buildx
-  //   await exec.exec('docker', [
-  //     'run',
-  //     '--rm',
-  //     '--privileged',
-  //     'docker/binfmt:820fdd95a9972a5308930a2bdfb8573dd4447ad3',
-  //   ]);
-
-  //   await exec.exec('cat', [
-  //     '/proc/sys/fs/binfmt_misc/qemu-aarch64',
-  //   ]);
-
-  //   await exec.exec('docker', [
-  //     'buildx',
-  //     'create',
-  //     '--use',
-  //     '--name',
-  //     'mybuilder',
-  //   ]);
-
-  //   await exec.exec('docker', [
-  //     'buildx',
-  //     'inspect',
-  //     '--bootstrap'
-  //   ]);
-  // }, () => {
-  //   core.debug('NOT Support Buildx');
-  // });
 }
 
 run().then(() => {
