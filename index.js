45c440e6a4d3c03aa52051bc28eb3a75ad5212d9
Nick Fields <nickfields@invisionapp.com>
2019-11-20 11:00:27 -0500

    .

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 const exec = require('@actions/exec');
 const core = require('@actions/core');
-const git = require('simple-git');
 
 const GITHUB_TOKEN = core.getInput('github-token', {required: true});
 const GITHUB_REPO = core.getInput('github-repo', {required: true});
+const GITHUB_BRANCH = core.getInput('github-branch', {required: false});
+
+const DEFAULT_BRANCH = 'master'
+const WORKING_DIR = './.private-action'
 
 async function run() {
+  const repo = `https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git`
+  const cmd = [
+    'git clone', 
+    '--single-branch', 
+    '--no-tags', 
+    '--depth 1', 
+    `-b ${GITHUB_BRANCH || DEFAULT_BRANCH}`, 
+    repo,  
+    WORKING_DIR
+  ].join(' ')
+
+  core.info('Cloning private action')
+  await exec.exec(cmd);
 
   // const envs = Object.keys(process.env).filter(e=>e.startsWith('INPUT_'));
   const cwd = process.cwd();
-  const wd = __dirname;
-
-  // core.debug(envs)
   
   console.log('cwd')
   console.log(cwd)
-
-  console.log('dirname')
-  console.log(wd)
 }
 
 run().then(() => {
